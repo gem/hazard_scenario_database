f249d07316f5ca26ef68fc8516cfbf12fc96c718
Matteo Nastasi <nastasi@openquake.org>
2018-02-07 14:42:28 +0100

    latest code from MP

---
+++ python/mhs/earthquake_scenarios.py
@@ -0,0 +1,125 @@
+
+import re
+import numpy as np
+
+from datetime import date
+from mhs import Footprint, Event, EventSet
+
+
+class SitesCsv():
+
+    def __init__(self,  idxs=None, coords=None,):
+        self.coords = coords
+        self.idxs = idxs
+
+    @classmethod
+    def from_csv_file(cls, csv_fname):
+        """
+        :param csv_fname:
+        """
+        coords = []
+        idxs = []
+        for i, line in enumerate(open(csv_fname)):
+            if i > 0:
+                aa = re.split('\,', line)
+                coords.append([float(aa[1]), float(aa[2])])
+                idxs.append(int(aa[0]))
+        print('The file contains {:d} sites'.format(len(idxs)))
+        return cls(np.array(idxs), np.array(coords))
+
+
+class GmfCsv():
+    """
+    :param list gmfs:
+        A collection of np.arrays
+    """
+    def __init__():
+        gmfs = None
+
+    @classmethod
+    def from_csv_file(cls, csv_fname, sites):
+        """
+        """
+        #
+        # loading data from the csv file
+        data = []
+        for i, line in enumerate(open(csv_fname)):
+            aa = re.split('\,', line)
+            if i == 0:
+                labels = aa
+            else:
+                data.append([d for d in aa])
+        data = np.array(data)
+        #
+        # get the IDs of the gmfs
+        gmfids = set(list(data[:, 2]))
+        print('The file contains {:d} GMFs'.format(len(gmfids)))
+        #
+        # storing data for the various IMTs
+        gmfs = {}
+        for ii, ia in enumerate(range(3, len(labels))):
+            # if ii > 0:
+            #     continue
+            lab = re.sub('^gmv_', '', labels[ia])
+            gmfs[lab] = []
+            print(lab)
+            footprints = []
+            for igm in gmfids:
+                iii = np.nonzero(data[:, 2].astype(int) == int(igm))
+                tsit = np.array(data[iii, ia]).T
+                tdat = np.squeeze(sites.coords[data[iii, 1].astype(int), :])
+                xxx = np.hstack((tsit, tdat))
+                footp = Footprint(fid=igm,
+                                  event_id='e{:d}'.format(ii+1),
+                                  data=xxx,
+                                  imt=lab,
+                                  process_type='e-gm',
+                                  data_uncertainty_distribution=None,
+                                  data_uncertainty_2nd_moment=None,
+                                  data_uncertainty_values=None,
+                                  triggering_footprint_id=None)
+                footprints.append(footp)
+
+            event = Event(eid='e{:d}'.format(ii+1),
+                          scenario_set_id='es1',
+                          calculation_method='simulated',
+                          frequency=1./475.,
+                          occurrence_prob=None,
+                          occurrence_time_start=None,
+                          occurrence_time_end=None,
+                          occurrence_time_span=None,
+                          trigger_peril_type=None,
+                          trigger_process_type=None,
+                          trigger_event_id=None,
+                          description='Test footprints',
+                          footprints=footprints)
+
+        descr = 'Sample scenarios for Dodoma, Tanzania'
+        eventset = EventSet(esid='es1',
+                            geographic_area_bb=[-9., 33., -3., 39.],
+                            geographic_area_name='Dodoma, Tanzania',
+                            creation_date=date(2018, 1, 30).isoformat(),
+                            peril_type='earthquake',
+                            time_start=None,
+                            time_end=None,
+                            time_duration=None,
+                            description=descr,
+                            bibliography=None,
+                            events=[event])
+        return eventset
+
+
+def main():
+
+    fname = './../../scenarios/Earthquakes/20180117/sitemesh-_17001.csv'
+    sites = SitesCsv.from_csv_file(fname)
+
+    fname = './../../scenarios/Earthquakes/20180117/gmf-data_17001.csv'
+    es = GmfCsv.from_csv_file(fname, sites)
+
+    fname_out = 'sample.json'
+    es.to_json(fname_out)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/mhs/mhs.py
@@ -0,0 +1,143 @@
+
+import json
+from json import JSONEncoder
+
+
+PERIL_TYPES = ['earthquake', 'flood', 'landslide', 'storm_surge', 'tsunami',
+               'volcano']
+
+PROCESS_TYPE = {'earthquake': ['ground shaking', 'primary_surface_rupture',
+                               'secondary_surface_rupture', 'liquefaction'],
+                'flood': ['inundation'],
+                'landslide': ['fall', 'topple', 'slide', 'lateral_spread',
+                              'flow', 'complex'],
+                'storm_surge': ['inundation'],
+                'volcano': ['ash_fall', 'airborne_ash', 'lahar']}
+
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o._asdict()
+
+
+class EventSet():
+    """
+    Event Set
+
+    :param eisd:
+    :param geographic_area_bb:
+    :param geographic_area_name:
+    :param creation_date:
+    :param peril_type:
+    :param time_start:
+        ISO 8601 formatted
+    :param time_end:
+        ISO 8601 formatted
+    :param str time_duration:
+        ISO 8601 formatted
+    :param str description:
+    :param str bibliography:
+    """
+
+    def __init__(self, esid, geographic_area_bb, geographic_area_name,
+                 creation_date, peril_type, time_start=None,
+                 time_end=None, time_duration=None, description=None,
+                 bibliography=None, events=None):
+        self.esid = esid
+        self.geographic_area_bb = geographic_area_bb
+        self.geographic_area_name = geographic_area_name
+        self.creation_date = creation_date
+        self.peril_type = peril_type
+        self.time_start = time_start
+        self.time_end = time_end
+        self.time_duration = time_duration
+        self.description = description
+        self.bibliography = bibliography
+        self.events = events
+
+    def to_json(self, fname):
+        """
+        :param str fname:
+            The name of the file where to write json
+        :returns:
+            A JSON formatted string with the content of the eventset
+        """
+        fou = open(fname, 'w')
+        json.dump(self, fou, cls=MyEncoder, allow_nan=True, indent=4)
+        fou.close()
+
+    def _asdict(self):
+        return self.__dict__
+
+
+class Event():
+    """
+    Event
+    """
+
+    def __init__(self, eid, scenario_set_id, calculation_method=None,
+                 frequency=None, occurrence_prob=None,
+                 occurrence_time_start=None, occurrence_time_end=None,
+                 occurrence_time_span=None, trigger_peril_type=None,
+                 trigger_process_type=None, trigger_event_id=None,
+                 description=None, footprints=None):
+        """
+        :param list footprints:
+        """
+        self.eid = eid
+        self.scenario_set_id = scenario_set_id
+        self.calculation_method = calculation_method
+        self.frequency = frequency,
+        self.occurrence_prob = occurrence_prob,
+        self.occurrence_time_start = occurrence_time_start,
+        self.occurrence_time_end = occurrence_time_end,
+        self.occurrence_time_span = occurrence_time_span,
+        self.trigger_peril_type = trigger_peril_type,
+        self.trigger_process_type = trigger_process_type,
+        self.trigger_event_id = trigger_event_id,
+        self.description = description
+        self.footprints = footprints
+
+    def _asdict(self):
+        return self.__dict__
+
+
+class Footprint():
+    """
+    Footprint
+
+    :param str fid:
+        Footprint ID
+    :param str event_id:
+        String identifying the corresponding event
+    :param data:
+        A :class:`numpy.ndarray` instance
+    :param str imt:
+        The string defining the intensity measure type
+    :param str process_type:
+        The key describing the typology of process modelled (e.g. ground
+        motion)
+    :param data_uncertainty_distribution:
+    :param data_uncertainty_2nd_moment:
+    :param data_uncertainty_values:
+    :param triggering_footprint_id:
+    """
+
+    def __init__(self, fid, event_id, data, imt, process_type,
+                 data_uncertainty_distribution, data_uncertainty_2nd_moment,
+                 data_uncertainty_values, triggering_footprint_id):
+        self.fid = fid
+        self.event_id = event_id
+        # self.data = data
+        self.data = data
+        self.imt = imt
+        self.process_type = process_type
+        self.data_uncertainty_distribution = data_uncertainty_distribution
+        self.data_uncertainty_2nd_moment = data_uncertainty_2nd_moment
+        self.data_uncertainty_values = data_uncertainty_values
+        self.triggering_footprint_id = triggering_footprint_id
+
+    def _asdict(self):
+        tmpd = self.__dict__
+        tmpd['data'] = tmpd['data'].tolist()
+        return tmpd
