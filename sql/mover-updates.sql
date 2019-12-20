34afe8770bc99c0d1661089b922ecd5596b3f06d
Paul Henshaw <paul@openquake.org>
2019-12-20 10:51:20 +0100

    Updated comments

--- sql/mover-updates.sql
+++ sql/mover-updates.sql
@@ -2,11 +2,11 @@
 
 -- Add a project field
 ALTER TABLE hazard.contribution ADD COLUMN IF NOT EXISTS project VARCHAR;
--- All hazard contributions with id<=90 come from the SWIO RAFI project
+-- Set project name for contributions from the SWIO RAFI project
 UPDATE hazard.contribution SET project = 'SWIO RAFI' 
   WHERE model_source LIKE '%SWIO RAFI%';
 
--- Add a contributed_at timestamp (with time zone)
+-- Add a contributed_at timestamp
 ALTER TABLE hazard.contribution ADD COLUMN IF NOT EXISTS 
 	contributed_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW();
 UPDATE hazard.contribution SET contributed_at = '2019-04-01 00:00:00+0' 
